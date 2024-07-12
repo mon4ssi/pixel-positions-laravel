0e21a259711d18a0a6912054df0620a211750c6b
Monassi Carrasco <mon4ssi@outlook.com>
2024-07-12 16:33:59 -0500

    feat: add sentry for js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
+import * as Sentry from '@sentry/browser';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+});
