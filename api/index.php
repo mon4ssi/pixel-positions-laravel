c857810dc16f7f3856c7fe9c41f30a41008d1014
Monassi Carrasco <mon4ssi@outlook.com>
2024-07-12 12:41:01 -0500

    feat: add vercel integration for deployment

---
+++ api/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Vercel Integration
+|--------------------------------------------------------------------------
+|
+| Unfortunately, Vercel only allows an app’s entry-point to live inside the
+| public directory. So, we’ll need to redirect any requests to the
+| /api/index.php route to the /public/index.php entry-point.
+|
+*/
+
+require __DIR__.'/../public/index.php';
